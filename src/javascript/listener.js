ad6fa21eb3e2abd2f28b8fe5987b96d5bffe687c
Léo <leogianfagna@gmail.com>
2024-08-24 11:50:26 -0300

    Fix path de páginas do listener

--- src/javascript/listener.js
+++ src/javascript/listener.js
@@ -1,7 +1,7 @@
 document.getElementById('webhookEventos').addEventListener('click', function() {
-    window.location.href = '/src/html/sendEvent.html';
+    window.location.href = 'src/html/sendEvent.html';
 });
 
 document.getElementById('webhookStatus').addEventListener('click', function() {
-    window.location.href = '/src/html/status.html';
+    window.location.href = 'src/html/status.html';
 });
